864f0168b2b8c115c2e1549171e2575170ad34bc
Yukun Ma <gmayukun@gmail.com>
2018-04-12 12:18:44 +0800

    快写不完了

--- parser.py
+++ parser.py
@@ -2,73 +2,151 @@
 
 from enum import Enum
 
-Code = Enum('Code', ('Variable', '', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'))
+Code = Enum('Code', ('Variable', 'int', '*', "=", '#'))
 
-class Variable:
+class Rule:
+    def __init__(self, L, R):
+        self.L = L
+        self.R = R
+        print("new Rule:", L, end=' --> [')
+        for i in R:
+            print(i, end=' ')
+        print(']')
+
+class Element:
+    def __init__(self, name):
+        self.name = name
+        if name[0] == '[':
+            self.code = Code.Variable
+        elif name[0] == '"':
+            for code in Code:
+                if code.name == name[1:-1]:
+                    self.code = code
+    def __str__(self):
+        return str((self.code, self.name))
+    def __hash__(self):
+        return self.name
+    def __eq__(self, other):
+        return self.name == other.name
+
+class ContextFreeLanguage:
+
     def __init__(self):
-        pass
+        self.rules = []
+        self.elementsDict = {}
+        self.elements = []
+        self.starter = ""
+        self.ender = ""
 
-class ProductionRule:
-    class Element:
-        def __init__(self, name):
-            self.name = 0
-            if name[0] == '[':
-                self.code = Code.Variable
-            elif name[0] == '"':
-                for code in Code:
-                    if code.name == name[1:-1]:
-                        self.code = code
-    def __init__(self, strL, strR):
+    def addElement(self, eleStr):
+        if eleStr not in self.elementsDict:
+            self.elements.append(Element(eleStr))
+            self.elementsDict[eleStr] = self.elements[-1]
+        return self.elementsDict[eleStr]
+
+    def addRule(self, strL, strR):
+        L = 0
         for i in range(len(strL)):
             if strL[i] == '[':
                 j = i + 1
-                while strR[j] != ']':
+                while strL[j] != ']':
                     j += 1
-                self.L = Element(strL[i+1:j], VAR)
+                L = strL[i:j+1]
 
-        self.R = []
+        R = []
         i = 0
         while i < len(strR):
-            if strL[i] == '[':
+            if strR[i] == '[':
                 j = i + 1
                 while strR[j] != ']':
                     j += 1
-                self.R.append(Element(strR[i:j+1]))
-                i = j+1
-            elif strR[i] == '(':
-                j = i + 1
-                while strR[j] != ')':
-                    j += 1
-                self.R.append(Element(strR[i:j+1]))
+                R.append(self.addElement(strR[i:j+1]))
                 i = j+1
             elif strR[i] == '"':
                 j = i + 1
                 while strR[j] != '"':
                     j += 1
-                self.R.append(Element(strR[i:j+1]))
+                R.append(self.addElement(strR[i:j+1]))
                 i = j+1
             else:
                 i += 1
+        self.rules.append(Rule(L, R))
+        return self.rules[-1]
+    def addStarter(self, strL):
+        self.starter = self.addElement(strL)
+        print(self.starter)
+        return self.starter
+    def addEnder(self, strL):
+        self.ender = self.addElement(strL)
+        print(self.ender)
+        return self.ender
 
+    '''
+    Item is a tuple:
+    (A->x.By, a) := ((A->xBy), 1, a)
+    '''
 
-class Parser:
-    def __init__(self):
-        self.var = dict()
-        self.dest = dict()
-    def addVar(self, newVar):
-        var.add(newVar)
-        if var.get(newVar) == None:
-            var[newVar] = Variable()
-    def addDest(self, newDest):
-        dest.add(newDest)
-    def addStatement(self, nweProd):
-        prod.add(newProd)
+    def closure(self, I):
+        C = set(I)
+        newItem = True
+        while newItem:
+            flag = False
+            for c in C:
+                if c[0].R[c[1]].code != Code.Variable:
+                    continue
+                f = first(c[0].R[c[1]+1:].append(c[2]))
+                for r in rules:
+                    if r.L == c:
+                        for b in f:
+                            if (r, 0, b) not in C:
+                                flag = True
+                                C.add((r, 0, b))
+            newItem = flag
+        return frozenset(C)
+
+    def init(self):
+        pass
+
+    def parse(self, sentence):
+        i = 0
+        s = []
+        while i < len(sentence):
+            if sentence[i] == '[':
+                j = i + 1
+                while sentence[j] != '[':
+                    j += 1
+                s.append(self.elementsDict[sentence[i:j+1]])
+                i = j + 1
+            elif sentence[i] == '"':
+                j = i + 1
+                while sentence[j] != '"':
+                    j += 1
+                s.append(self.elementsDict[sentence[i:j+1]])
+                i = j + 1
+            else:
+                i += 1
+        for w in s:
+            print(w)
+
 
 if __name__ == "__main__" :
-    f = open("test_grammar.g", "r")
+    grammarFile = open("test_grammar.g", "r")
+    cfl = ContextFreeLanguage()
+
     while True:
-        line = f.readline()
-        if line == "\n" or line == "":
+        line = grammarFile.readline()
+        if line == "":
             break
         [L,R] = line[:-1].split('-->')
-        print(L, R)
+        print("input", L, R)
+        cfl.addRule(L, R)
+    cfl.addRule("[S_p]", "[S]")
+    cfl.addStarter("[S_p]")
+    cfl.addEnder('"#"')
+
+    testFile = open("test_file.g", "r")
+    while True:
+        line = testFile.readline()[:-1]
+        if line == "":
+            break
+        cfl.parse(line + '"#"')
